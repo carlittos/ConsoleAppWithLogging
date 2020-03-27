a65783a75b8b16d7719ba2d5b44111be3d0917b0
Carlos Reis <carlittos@msn.com>
2020-03-27 19:30:42 -0400

    Added Entry Point Service

---
+++ ConsoleAppWithLogging/ConsoleAppWithLogging.UI/Services/EntryPointService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithLogging.UI.Services
+{
+    public class EntryPointService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<EntryPointService> _logger;
+
+        public EntryPointService(IConfiguration configuration, ILogger<EntryPointService> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task Run()
+        {
+            _logger.LogInformation("Process started at {Time}", DateTimeOffset.Now);
+            await Task.Delay(1000);
+            _logger.LogInformation("Process stopped at {Time}", DateTimeOffset.Now);
+        }
+    }
+}
